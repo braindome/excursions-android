401ec14811e277dd7ae034319e1160df99de3019
Antonio Saracino <antonio.saracino83@gmail.com>
2024-02-27 00:28:59 +0100

    Fetch request done

--- app/src/main/java/com/example/excursions/ExcursionsApp.kt
+++ app/src/main/java/com/example/excursions/ExcursionsApp.kt
@@ -1,12 +1,44 @@
 package com.example.excursions
 
 import android.app.Application
-import androidx.navigation.compose.rememberNavController
+import com.example.excursions.api.ExcursionsAPI
 import com.google.android.libraries.places.api.Places
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class ExcursionsApp : Application() {
+    private val placesBaseUrl = "https://places.googleapis.com/v1/"
 
+    val api:ExcursionsAPI by lazy {
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor { chain: Interceptor.Chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("X-Goog-Api-Key", BuildConfig.PLACES_API_KEY)
+                    .addHeader("X-Goog-FieldMask", "places.formattedAddress,places.displayName,places.id,places.location,places.primaryType,places.types")
+                    .build()
+                chain.proceed(request)
+            }
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(placesBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(httpClient)
+            .build()
+
+        retrofit.create(ExcursionsAPI::class.java)
+    }
     override fun onCreate() {
         super.onCreate()
 
@@ -21,6 +53,9 @@ class ExcursionsApp : Application() {
             //finish()
             return
         }
+
+
+
 
 
         // Initialize the SDK
---
+++ app/src/main/java/com/example/excursions/data/api_models/SearchNearbyResponse.kt
@@ -0,0 +1,32 @@
+package com.example.excursions.data.api_models
+
+import com.google.gson.annotations.JsonAdapter
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SearchNearbyResponse(
+    @Json(name = "places") val places: List<Place>
+)
+
+@JsonClass(generateAdapter = true)
+data class Place(
+    @Json(name = "displayName") val displayName: DisplayName,
+    @Json(name = "formattedAddress") val formattedAddress: String,
+    @Json(name = "id") val id: String,
+    @Json(name = "location") val location: Location,
+    @Json(name = "primaryType") val primaryType: String?,
+    @Json(name = "types") val types: List<String>
+)
+
+@JsonClass(generateAdapter = true)
+data class DisplayName(
+    @Json(name = "languageCode") val languageCode: String,
+    @Json(name = "text") val text: String
+)
+
+@JsonClass(generateAdapter = true)
+data class Location(
+    @Json(name = "latitude") val latitude: Double,
+    @Json(name = "longitude") val longitude: Double
+)
